543719948d8ac58298f5ae49766c83acbb9702fe
jubarrossousa10 <juliana.barros@poli.ufrj.br>
2022-07-04 18:16:37 -0300

    versão pronta com tratamento

--- calculadora_angulo.py
+++ calculadora_angulo.py
@@ -12,20 +12,46 @@ def calcAng (hora, minutos):
     angMin = minutos * 6
 
     angTotal = abs (angHora - angMin)
+
+    # Calculando o seu replemento, e escolhendo ele caso seja menor
     angTotal2 = 360 - angTotal
+
     return (min (angTotal, angTotal2))
+
+class Erro (Exception):
+    #Classe base para outras exceções
+    pass
+
+class FormatoErrado(Erro):
+    #Erro para quando o formato da entrada é inadequado
+    pass
 
 def main ():
 
     horario = input ("Entre com o horario: ")
-    print(horario)
+
     if horario.upper() == "F":
         return (print ("fim do programa"))
 
-    [hora, minutos] = horario.split(":")
-    hora = int(hora)
-    minutos = int(minutos)
-    angulo = calcAng (hora, minutos)
-    print (angulo)
+    try:
+        [hora, minutos] = horario.split(":")
+
+        if len (hora) == 2 and len(minutos) == 2:
+            hora = int(hora)
+            minutos = int(minutos)
+            
+            if (hora < 25) and (minutos < 60):
+                angulo = calcAng (hora, minutos)
+                print ("O ângulo entre os ponteiros é: ", angulo)
+            else:
+                raise FormatoErrado
+
+        else:
+            raise FormatoErrado
+    
+    except:
+        print("Erro: Formato do horário inválido")
+        print("Entre com uma entrada no formato- hh:mm")
+
 
 main ()
