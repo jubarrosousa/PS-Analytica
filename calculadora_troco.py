ec4b6d97ea48e993fd4f1849566c27cd762e14cb
jubarrossousa10 <juliana.barros@poli.ufrj.br>
2022-07-03 23:13:40 -0300

    primeira versão, sem comentários ou tratamento

---
+++ calculadora_troco.py
@@ -0,0 +1,12 @@
+### terceira atividade de programação P2 ##################
+############ calculadora de troco #########################
+
+n1 = 576.73
+n = 576.73
+
+nu = [100,50,20,10,5,2,1,0.5,0.25,0.1,0.05, 0.01]
+total =[]
+for cedulas in nu:
+ total.append[int(n//cedulas)]
+ n = n%cedulas
+print (nu)
